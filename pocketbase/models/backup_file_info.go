881e3349655df10e0ec67de52d65c66367fb5f1d
Chris <chris@breadchris.com>
2025-06-17 19:14:18 -0700

    remove pocketbase

--- pocketbase/models/backup_file_info.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/breadchris/share/pocketbase/tools/types"
-
-type BackupFileInfo struct {
-	Key      string         `json:"key"`
-	Size     int64          `json:"size"`
-	Modified types.DateTime `json:"modified"`
-}
